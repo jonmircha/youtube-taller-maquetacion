431063a40b8c1b50d10a3ac22b3ccdcbc0be48ef
Jonathan MirCha <jonmircha@gmail.com>
2020-12-25 21:11:19 -0600

    Subiendo Proyecto 1 LandingPage Portafolio-CV

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Taller de Maquetación
 
 Repositorio de proyectos del taller de maquetación de @jonmircha en YouTube
+
+## Proyectos
+
+- [Landing Page de tipo Portafolio-CV](https://jonmircha.github.io/youtube-taller-maquetacion/portafolio-cv)
---
+++ portafolio-cv/script.js
@@ -0,0 +1,55 @@
+/* ********** Menu ********** */
+((d) => {
+  const $btnMenu = d.querySelector(".menu-btn"),
+    $menu = d.querySelector(".menu");
+
+  $btnMenu.addEventListener("click", (e) => {
+    $btnMenu.firstElementChild.classList.toggle("none");
+    $btnMenu.lastElementChild.classList.toggle("none");
+    $menu.classList.toggle("is-active");
+  });
+
+  d.addEventListener("click", (e) => {
+    if (!e.target.matches(".menu a")) return false;
+
+    $btnMenu.firstElementChild.classList.remove("none");
+    $btnMenu.lastElementChild.classList.add("none");
+    $menu.classList.remove("is-active");
+  });
+})(document);
+
+/* ********** ContactForm ********** */
+((d) => {
+  const $form = d.querySelector(".contact-form"),
+    $loader = d.querySelector(".contact-form-loader"),
+    $response = d.querySelector(".contact-form-response");
+
+  $form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    $loader.classList.remove("none");
+    fetch("https://formsubmit.co/ajax/jonmircha@gmail.com", {
+      method: "POST",
+      body: new FormData(e.target),
+    })
+      .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+      .then((json) => {
+        console.log(json);
+        location.hash = "#gracias";
+        $form.reset();
+      })
+      .catch((err) => {
+        console.log(err);
+        let message =
+          err.statusText || "Ocurrió un error al enviar, intenta nuevamente";
+        $response.querySelector(
+          "h3"
+        ).innerHTML = `Error ${err.status}: ${message}`;
+      })
+      .finally(() => {
+        $loader.classList.add("none");
+        setTimeout(() => {
+          location.hash = "#close";
+        }, 3000);
+      });
+  });
+})(document);
